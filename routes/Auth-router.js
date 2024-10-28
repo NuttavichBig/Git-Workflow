a23851b35c3ffb6abc1c41a1dcaef96b8dc6db12
NuttavichBig <nuttavich.si@gmail.com>
2024-10-28 15:45:05 +0700

    some change

--- routes/Auth-router.js
+++ routes/Auth-router.js
@@ -3,5 +3,6 @@ const authRoute = express.Router();
 
 authRoute.post('/login', (req, res) => {})
 authRoute.post('/register', (req, res) => {})
+authRoute.get('/getMe',(req,res)=>{})
 
 module.exports = authRoute
