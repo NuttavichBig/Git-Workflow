c137ff37db8d38b7e34ca1ea24d19cf55cac6576
NuttavichBig <nuttavich.si@gmail.com>
2024-10-28 14:16:42 +0700

    Start Project

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require("express")
+const app = express();
+
+
+
+app.use(cors({}))
+
+const port = process.env.PORT
+app.listen(port,()=>{
+    console.log("server running on port",port)
+})
