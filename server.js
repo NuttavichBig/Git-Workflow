7b4ae0229ad8bb09fa7bc5fc83056cc056537e6a
NuttavichBig <nuttavich.si@gmail.com>
2024-10-28 15:18:43 +0700

    user feature complete

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
-const express = require("express")
+const express = require("express");
+const userRoute = require('./routes/userRoute');
 const app = express();
 
 
 
-app.use(cors({}))
+app.use(cors())
+
+
+
+app.use('/user',userRoute)
+
 
 const port = process.env.PORT
 app.listen(port,()=>{
