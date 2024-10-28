5e363142e6fb655f4f9ae4e24491e2c912fd04e8
Ginwobb <suphapinya.smj@gmail.com>
2024-10-28 15:19:07 +0700

    AuthDone

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
-const express = require("express")
+const express = require("express");
+const authRoute = require('./routes/Auth-router');
 const app = express();
 
-
-
 app.use(cors({}))
+
+app.use('/auth',authRoute)
 
 const port = process.env.PORT
 app.listen(port,()=>{
