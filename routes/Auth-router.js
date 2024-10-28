1b47d2deb0ed391d783409fcd0416af56341ad36
Ginwobb <suphapinya.smj@gmail.com>
2024-10-28 15:37:41 +0700

    edit name

--- routes/Auth-router.js
+++ routes/Auth-router.js
@@ -2,5 +2,6 @@ const express = require("express");
 const authRoute = express.Router();
 
 authRoute.post('/login', (req, res) => {})
+authRoute.post('/register', (req, res) => {})
 
 module.exports = authRoute
